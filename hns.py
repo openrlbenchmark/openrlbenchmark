2b8fb919440b6a9f093b940e9698f458a889b410
Costa Huang <costa.huang@outlook.com>
2022-08-07 22:47:57 -0400

    update hns

--- hns.py
+++ hns.py
@@ -4,9 +4,16 @@ import pandas as pd
 import wandb
 import wandb.apis.reports as wb  # noqa
 from expt import Hypothesis, Run
+from typing import cast
 from expt.plot import GridPlot
-
 from atari_data import atari_human_normalized_scores
+
+
+def repr_fn(h: Hypothesis) -> pd.DataFrame:
+    # A dummy function that manipulates the representative value ('median')
+    df = cast(pd.DataFrame, h.grouped.median())
+    # df['loss'] = np.asarray(df.reset_index()['step']) * -1.0
+    return df
 
 
 def create_expt_runs(wandb_runs):
@@ -22,78 +29,101 @@ def create_expt_runs(wandb_runs):
 api = wandb.Api()
 
 env_ids = atari_human_normalized_scores.keys()
-y_names = ["Median human normalized score", "Median human normalized score 2", "Mean human normalized score"]
+y_names = ["Median human normalized score", "Mean human normalized score"]
 
-
+NUM_SEEDS = 3
 NUM_FRAME_STACK = 4
-runss = []
-for env_id in env_ids:
-    api = wandb.Api()
-    wandb_runs = api.runs(
-        path="costa-huang/envpool-atari",
-        filters={"$and": [{"config.env_id.value": env_id}, {"config.exp_name.value": "ppo_atari_envpool_xla_jax"}]},
+median_runss = []
+mean_runss = []
+for seed in range(1, NUM_SEEDS+1):
+    runss = []
+    for env_id in env_ids:
+        api = wandb.Api()
+        wandb_runs = api.runs(
+            path="openrlbenchmark/envpool-atari",
+            filters={"$and": [{"config.env_id.value": env_id}, {"config.exp_name.value": "ppo_atari_envpool_xla_jax"}, {"config.seed.value": seed}]},
+        )
+        expt_runs = create_expt_runs(wandb_runs)
+
+        # normalize scores and adjust x-axis from steps to frames
+        for expt_run in expt_runs:
+            expt_run.df["charts/avg_episodic_return"] = (
+                expt_run.df["charts/avg_episodic_return"] - atari_human_normalized_scores[env_id][0]
+            ) / (atari_human_normalized_scores[env_id][1] - atari_human_normalized_scores[env_id][0])
+            expt_run.df["global_step"] *= NUM_FRAME_STACK
+        runss.extend(expt_runs)
+
+    
+    ex = expt.Experiment("Comparison of PPO")
+    ex.add_runs("CleanRL's PPO", runss)
+    figure = ex.plot(
+        x="global_step",
+        y="charts/avg_episodic_return",
+        rolling=50,
+        n_samples=400,
+        legend=False,
+        err_fn=None,
+        err_style=None,
+        suptitle="",
+        title=y_names[0],
+        representative_fn=repr_fn,
+        # ax=g[y_names[0]],
     )
-    expt_runs = create_expt_runs(wandb_runs)
-
-    # normalize scores and adjust x-axis from steps to frames
-    for expt_run in expt_runs:
-        expt_run.df["charts/avg_episodic_return"] = (
-            expt_run.df["charts/avg_episodic_return"] - atari_human_normalized_scores[env_id][0]
-        ) / (atari_human_normalized_scores[env_id][1] - atari_human_normalized_scores[env_id][0])
-        expt_run.df["global_step"] *= NUM_FRAME_STACK
-    runss.extend(expt_runs)
-
-
-from typing import cast
-
-
-def repr_fn(h: Hypothesis) -> pd.DataFrame:
-    # A dummy function that manipulates the representative value ('median')
-    df = cast(pd.DataFrame, h.grouped.median())
-    # df['loss'] = np.asarray(df.reset_index()['step']) * -1.0
-    return df
-
+    ax = figure.axes[0,0]
+    median_runss.extend([
+        Run(f"seed-{seed}",
+        pd.DataFrame({
+            "global_step": ax.lines[0].get_xdata(),
+            "median_human_normalized_score": ax.lines[0].get_ydata(),
+        },
+    ))])
+    figure = ex.plot(
+        x="global_step",
+        y="charts/avg_episodic_return",
+        rolling=50,
+        n_samples=400,
+        legend=False,
+        err_fn=lambda h: h.grouped.sem(),
+        err_style=None,
+        title=y_names[1],
+        suptitle="",
+    )
+    ax = figure.axes[0,0]
+    mean_runss.extend([
+        Run(f"seed-{seed}",
+        pd.DataFrame({
+            "global_step": ax.lines[0].get_xdata(),
+            "mean_human_normalized_score": ax.lines[0].get_ydata(),
+        },
+    ))])
 
 g = GridPlot(y_names=y_names)
-ex = expt.Experiment("Comparison of PPO")
-ex.add_runs("CleanRL's PPO", runss)
+ex = expt.Experiment("Median human normalized scores")
+ex.add_runs("CleanRL's PPO", median_runss)
 ex.plot(
     x="global_step",
-    y="charts/avg_episodic_return",
+    y="median_human_normalized_score",
     rolling=50,
     n_samples=400,
     legend=False,
-    err_fn=None,
-    err_style=None,
+    # err_style="band",
     suptitle="",
     title=y_names[0],
-    representative_fn=repr_fn,
     ax=g[y_names[0]],
 )
+
+ex = expt.Experiment("mean human normalized scores")
+ex.add_runs("CleanRL's PPO", mean_runss)
 ex.plot(
     x="global_step",
-    y="charts/avg_episodic_return",
+    y="mean_human_normalized_score",
     rolling=50,
     n_samples=400,
     legend=False,
-    err_fn=None,
-    # err_style="fill",
+    # err_style="band",
     suptitle="",
     title=y_names[1],
-    representative_fn=repr_fn,
     ax=g[y_names[1]],
-)
-ex.plot(
-    x="global_step",
-    y="charts/avg_episodic_return",
-    rolling=50,
-    n_samples=400,
-    legend=False,
-    err_fn=lambda h: h.grouped.sem(),
-    err_style="fill",
-    title=y_names[2],
-    suptitle="",
-    ax=g[y_names[2]],
 )
 for ax in g:
     ax.yaxis.set_label_text("")
